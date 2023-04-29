f48bbf300535ac37905abc3c1ef6048a46d26bfa
OctikNy <octapikus@gmail.com>
2023-04-29 14:10:01 +0300

    add search by acf and menus

--- index.php
+++ index.php
@@ -5,36 +5,35 @@
 function search_by_slug($where, $query)
 {
     global $pagenow, $wpdb;
-    // var_dump($query);
     if (is_admin()  && isset($_GET['s']) && strpos($_GET['s'], 'slug:') !== false ) {
 
         $slug = str_replace('slug:', '', $_GET['s']);
 
-        $where = $wpdb->prepare(" AND post_name LIKE %s", '%' . $wpdb->esc_like($slug) . '%');
+        $where = $wpdb->prepare(" AND post_name LIKE %s", '' . $wpdb->esc_like($slug) . '');
  
     }
     
     return $where;
 }
-
 add_filter('posts_where', 'search_by_slug', 10, 2);
+add_action( 'pre_get_posts', 'menufilter', 10, 2 );
+function menufilter( $q ) {
 
-function search_by_slug2($query)
-{
-  
-    global $pagenow, $wpdb;
+    if(isset($_POST['action']) && $_POST['action']=="menu-quick-search" && isset($_POST['menu-settings-column-nonce'])){    
+        if( is_a($q->query_vars['walker'], 'Walker_Nav_Menu_Checklist') ){
+           
+            $slug = str_replace('slug:', '', $q->query_vars['s']);
+            $q->set( 's', $slug );
 
-    if (is_admin()  && isset($_GET['s']) && strpos($_GET['s'], 'slug:') !== false ) {
 
-        $slug = str_replace('slug:', '', $_GET['s']);
-
-        $where = $wpdb->prepare(" AND post_name LIKE %s", '%' . $wpdb->esc_like($slug) . '%');
-        $query->set('where', $where);
-        // return $where;
-            var_dump($query);
+        }
     }
-    
-    return $where;
+    return $q;
+}
+add_filter('acf/fields/post_object/query', 'acf_fields_post_object_query', 10, 3);
+function acf_fields_post_object_query( $args, $field, $post_id ) {
+    $slug = str_replace('slug:', '', $args['s']);
+    $args['s'] =  $slug ;
+    return $args;
 }
 
-// add_action( 'pre_get_posts', 'search_by_slug2' );
